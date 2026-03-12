96915cf9e1cc6aa5af7e5ac7641ec0aaea3bb8f3
gonapalikhitha <gonapalikhitha@gmail.com>
2026-03-12 21:50:10 +0530

    Leetcode problems

---
+++ GcdOddEven.java
@@ -0,0 +1,10 @@
+public class GcdOddEven {
+    public static int gcdOddEven(int n){
+        return n;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(gcdOddEven(n));
+    }
+    
+}
